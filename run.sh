e81484eaec98fd963f2054b9180f192b5b0caee2
TomeMD <tome.maseda@udc.es>
2023-03-12 13:15:46 +0100

    Moved secondary scripts to bin folder

--- run.sh
+++ run.sh
@@ -2,17 +2,16 @@
 
 # Set environment
 export STRESS_HOME=`dirname $0`
-export START_DATE=`date '+%d_%m_%Y_%H-%M-%S'`
-export OUT_DIR=${STRESS_HOME}/out/report_${START_DATE}
+export BIN_DIR=${STRESS_HOME}/bin
+export OUT_DIR=${STRESS_HOME}/out/report_$(date '+%d_%m_%Y_%H-%M-%S')
+mkdir -p $OUT_DIR
 export LOG_FILE=${OUT_DIR}/log
 
-mkdir -p $OUT_DIR
-
 # Load bash functions
-. ${STRESS_HOME}/functions.sh
+. ${BIN_DIR}/functions.sh
 
 # Parse arguments
-. ${STRESS_HOME}/parse-arguments.sh
+. ${BIN_DIR}/parse-arguments.sh
 
 # Run tests
-. ${STRESS_HOME}/run-tests.sh
+. ${BIN_DIR}/run-tests.sh
