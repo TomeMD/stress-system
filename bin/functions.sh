2dc18b1cdf86124ec372ce606c1d9ccd0098c73b
TomeMD <tome.maseda@udc.es>
2024-07-29 16:11:20 +0200

    Remove default execution on apptainer instance start

--- bin/functions.sh
+++ bin/functions.sh
@@ -47,7 +47,7 @@ export -f print_conf
 function run_test() {
     m_echo "Iteration ${3}: Stressing CPU core ${1} with ${2}% of load"
     echo "stress-ng --metrics --taskset ${1}${OTHER_OPTIONS} --cpu-load ${2} ${STRESSORS} --timeout ${TIMEOUT} >> ${CORE_FILE}_${1} 2>&1"
-    stress-ng --metrics --taskset ${1}${OTHER_OPTIONS} --cpu-load ${2} ${STRESSORS} --timeout ${TIMEOUT} >> "${CORE_FILE}_${1}" 2>&1
+    taskset -c ${1} stress-ng --metrics${OTHER_OPTIONS} --cpu-load ${2} ${STRESSORS} --timeout ${TIMEOUT} >> "${CORE_FILE}_${1}" 2>&1
 }
 
 export -f run_test
